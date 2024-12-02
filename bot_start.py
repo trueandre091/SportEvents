fc4f8cb5454da183e9990825eaa20fa4e54a09f2
andre <151407275+trueandre091@users.noreply.github.com>
2024-12-02 22:45:53 +0200

    Update bot_start.py

--- bot_start.py
+++ bot_start.py
@@ -4,15 +4,7 @@ from bot.bot import dp, bot
 from bot import handlers
 
 async def main():
-    # Создаем новый цикл событий
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    
-    try:
-        # Запускаем только поллинг бота, без парсинга
-        await dp.start_polling(bot)
-    finally:
-        loop.close()
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
