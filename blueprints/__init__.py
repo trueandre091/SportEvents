647bf44b1f88628141e4014a99feb861a3620dec
TopNik073 <borzovn169@gmail.com>
2024-12-05 01:48:44 +0200

    Add basic API for future frontend

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,4 +1,5 @@
 from blueprints.auth.auth import auth
 from blueprints.main.index import index
+from blueprints.api.api import api
 
-blueprints = [[auth, "/auth"], [index, "/"]]
+blueprints = [[auth, "/auth"], [index, "/"], [api, "/api"]]
---
+++ blueprints/api/api.py
@@ -0,0 +1,72 @@
+import logging
+
+from flask import Blueprint, request, jsonify
+from datetime import datetime
+
+from DB.event import Event
+
+api = Blueprint("api", __name__)
+logger = logging.getLogger(__name__)
+
+
+@api.post("/events")
+def api_get_events():
+    try:
+        sport = request.args.get("sport")
+        date_start = request.args.get("date_start")
+        date_end = request.args.get("date_end")
+        selected_date = request.args.get("date")
+        print(sport)
+        print(date_start)
+        print(date_end)
+        print(selected_date)
+        if selected_date:
+            selected_date = datetime.strptime(selected_date, '%Y-%m-%d')
+            events_objects = Event().get_events_by_date(selected_date)
+        else:
+            if date_start:
+                date_start = datetime.strptime(date_start, "%Y-%m-%d")
+            if date_end:
+                date_end = datetime.strptime(date_end, "%Y-%m-%d")
+            event = Event(sport=sport, date_start=date_start, date_end=date_end)
+            events_objects = event.get_by_filters()
+        events = []
+        for event in events_objects:
+            events.append({
+                'event_id': event.event_id,
+                'title': event.title,
+                'discipline': event.discipline,
+                'participants': event.participants,
+                'participants_num': event.participants_num,
+                'sport': event.sport,
+                'date_start': event.date_start.strftime('%Y-%m-%d') if event.date_start else None,
+                'date_end': event.date_end.strftime('%Y-%m-%d') if event.date_end else None,
+                'place': event.place
+            })
+
+        return jsonify(events)
+    except Exception as e:
+        logger.error(f"Error in get_events: {e}")
+        return jsonify([]), 500
+
+
+@api.post("/events/random")
+def api_get_random_events():
+    try:
+        event_manager: Event = Event()
+        random_events = [event_manager.event_to_dict(event) for event in event_manager.get_random_events()]
+
+        return jsonify(random_events)
+    except Exception as e:
+        logger.error(f"Error in get_random_events: {e}")
+        return jsonify([]), 500
+    
+
+@api.post("/events/sports")
+def api_get_sports():
+    try:
+        sports = Event().get_sports()
+        return jsonify(sports)
+    except Exception as e:
+        logger.error(f"Error in get_sports: {e}")
+        return jsonify([]), 500
