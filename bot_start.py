7e62138d8222c701bffb71a76f5633006627b550
TopNik073 <borzovn169@gmail.com>
2024-12-05 01:46:30 +0200

    Update: fix bot starting with parsing

--- bot_start.py
+++ bot_start.py
@@ -2,20 +2,11 @@ import asyncio
 
 from bot.bot import dp, bot
 from bot import handlers
-from bot import periodic_fn
 
 async def main():
-    # Создаем новый цикл событий
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    
-    try:
-        await asyncio.gather(
-            dp.start_polling(bot),
-            periodic_fn.parsing_and_notifications(test=False)
-        )
-    finally:
-        loop.close()
+    print("start polling")
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
