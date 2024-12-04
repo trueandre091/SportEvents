395e6fbdfebf087aaca3b9adefe7a2108179d3aa
TopNik073 <borzovn169@gmail.com>
2024-12-05 01:46:53 +0200

    Update: Event interface

--- DB/event.py
+++ DB/event.py
@@ -268,3 +268,23 @@ class Event:
         except Exception as e:
             print(f"Error in get_events_by_date: {e}")
             return []
+        
+    @staticmethod
+    def event_to_dict(event) -> dict | None:
+        try:
+            date_start = event.date_start.strftime('%Y-%m-%d') if event.date_start else None
+            date_end = event.date_end.strftime('%Y-%m-%d') if event.date_end else None
+            
+            result = {
+                'id': event.event_id,
+                'title': event.title,
+                'sport': event.sport,
+                'date_start': date_start,
+                'date_end': date_end,
+                'place': event.place
+            }
+            print("Преобразованное событие:", result)
+            return result
+        except Exception as e:
+            print(f"Ошибка при преобразовании события: {e}")
+            return None
